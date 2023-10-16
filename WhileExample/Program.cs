492701c9ca3e1c8f01c12a37062acfcb5897476c
Azra <yukselazrael@gmail.com>
2023-10-16 09:32:17 +0300

    While Example

---
+++ WhileExample/Program.cs
@@ -0,0 +1,72 @@
+﻿namespace WhileExample
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+
+
+            //int i = 0;
+            //while (i < 5) //Sınırlı döngü
+            //{
+            //    Console.WriteLine("While Döngüsü");
+            //    i++;
+            //}
+
+
+
+
+            while (true) //Sonsuz döngü olacak
+            {
+                string deger = Console.ReadLine();
+                if (deger == "exit")
+                {
+                    break;
+                }
+                Console.WriteLine(deger);
+            }
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+            Console.ReadLine();
+
+
+        }
+    }
+}
